f07c490c63d11e8f90578eaffa250c14e6690960
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2020-05-01 18:11:39 +0900

    chore: rename variable

--- slack.test.bash
+++ slack.test.bash
@@ -5,8 +5,8 @@ set -eu -o pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$script_dir"
 
-slack_api_port=8080
-export SLACK_BASE_URL=http://localhost:$slack_api_port
+api_port=8080
+export SLACK_BASE_URL=http://localhost:$api_port
 export SLACK_API_TOKEN=test-token
 
 
@@ -16,7 +16,7 @@ echo "case: post_text_message show help message"
 
 echo "case: post_text_message"
 (
-  req=$(echo -e "HTTP/1.1 200 OK\n\nOK" | busybox nc -l -p "$slack_api_port")
+  req=$(echo -e "HTTP/1.1 200 OK\n\nOK" | busybox nc -l -p "$api_port")
   echo "$req" | grep -qE "^POST /api/chat.postMessage HTTP/1.1"
   echo "$req" | grep -qE "^Authorization: Bearer ${SLACK_API_TOKEN}"
   body=$(echo "$req" | gawk '/^{/,/^}/')
@@ -29,7 +29,7 @@ wait "$(jobs -p)"
 
 echo "case: post_text_message with custom user name and user icon"
 (
-  req=$(echo -e "HTTP/1.1 200 OK\n\nOK" | busybox nc -l -p "$slack_api_port")
+  req=$(echo -e "HTTP/1.1 200 OK\n\nOK" | busybox nc -l -p "$api_port")
   body=$(echo "$req" | gawk '/^{/,/^}/')
   test "$(echo "$body" | jq -r .username)" = "Bash"
   test "$(echo "$body" | jq -r .icon_url)" = "http://localhost/icon.png"
@@ -41,7 +41,7 @@ wait "$(jobs -p)"
 
 echo "case: post_text_message fails when API returns 4xx"
 (
-  echo -e "HTTP/1.1 400 Bad Request\n\nBad Request" | busybox nc -l -p "$slack_api_port" > /dev/null
+  echo -e "HTTP/1.1 400 Bad Request\n\nBad Request" | busybox nc -l -p "$api_port" > /dev/null
 ) &
 if ./slack.bash post_text_message --channel "random" --text "Hello World!" > /dev/null; then
   echo "error: command should fail" >&2
@@ -51,7 +51,7 @@ wait "$(jobs -p)"
 
 echo "case: post_message"
 (
-  req=$(echo -e "HTTP/1.1 200 OK\n\nOK" | busybox nc -l -p "$slack_api_port")
+  req=$(echo -e "HTTP/1.1 200 OK\n\nOK" | busybox nc -l -p "$api_port")
   echo "$req" | grep -qE "^POST /api/chat.postMessage HTTP/1.1"
   echo "$req" | grep -qE "^Authorization: Bearer ${SLACK_API_TOKEN}"
   echo "$req" | grep -qE '"text": "Hello World!"'
