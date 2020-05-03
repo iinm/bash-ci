f2d0cfb1a065f6183c4a4c5029e1c509fedfd7c3
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2020-05-03 13:05:05 +0900

    test(hook_gitlab_merge_requests): trigger jenkins job

--- hook_gitlab_merge_requests.test.bash
+++ hook_gitlab_merge_requests.test.bash
@@ -75,3 +75,36 @@ HOOKS
 test "$exit_status" -ne 0
 test "$(cat ./tmp/hook_log/fail.001.log)" = "fail"
 test "$(cat ./tmp/hook_log/success.001.log)" = "success"
+
+
+echo "case: trigger jenkins job"
+# given:
+rm -rf ./tmp && mkdir ./tmp
+cat > ./tmp/mr.json << MR
+[
+  {
+    "iid": "1",
+    "title": "test mr 1",
+    "sha": "001",
+    "labels": [],
+    "source_branch": "source1",
+    "target_branch": "target1",
+    "web_url": "http://localhost/test1"
+  }
+]
+MR
+cat > ./tmp/hooks.tsv << 'HOOKS'
+jenkins-example	.labels | map(. == "skip-ci") | any | not	curl -X POST -u user:password "http://localhost:8080/job/test/build" -F json="$(./gitlab.bash merge_request_json_for_jenkins)"
+HOOKS
+(
+  # given:
+  req=$(echo -e "HTTP/1.1 200 OK" | busybox nc -l -p 8080)
+  # then:
+  echo "$req" | grep -qE '^POST /job/test/build HTTP/1.1'
+  echo "$req" | grep -q '{"parameter":[{"name":"MERGE_REQUEST_IID","value":"1"},{"name":"SOURCE_BRANCH","value":"source1"},{"name":"TARGET_BRANCH","value":"target1"},{"name":"MERGE_REQUEST_URL","value":"http://localhost/test1"}]}'
+) &
+request_validator_pid=$!
+# when:
+./hook_gitlab_merge_requests --logdir ./tmp/hook_log --merge-requests ./tmp/mr.json --hooks ./tmp/hooks.tsv
+# then:
+wait "$request_validator_pid"
