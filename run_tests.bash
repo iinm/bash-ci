b51cfc601424a5ffbddb85202df0f751d51a2e83
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2020-05-03 09:20:35 +0900

    feat: add test runner

---
+++ run_tests.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+help() {
+  cat << HELP
+Usage: run_tests.bash [--logfile FILE] TEST_SCRIPT...
+HELP
+}
+
+while test "$#" -gt 0; do
+  case "$1" in
+    --help    ) help; exit 0 ;;
+    --logfile ) logfile=$2; shift 2 ;;
+    *         ) break;
+  esac
+done
+
+: "${logfile:=test.log}"
+if test "$#" -eq 0; then
+  help >&2
+  exit 1
+fi
+
+echo > "$logfile"
+exit_status=0
+for file in "$@"; do
+  echo "$(tput setaf 4)test: $file$(tput sgr0)"
+  if bash -x "$file" 2>> "$logfile"; then
+    echo "$(tput setaf 2)all case passed$(tput sgr0)"
+  else
+    exit_status=$?
+    echo "$(tput setaf 1)failed$(tput sgr0)"
+    tac "$logfile" | grep -B 1000 -m 1 "+ echo 'case:" | tac
+  fi
+  echo
+done
+
+exit "$exit_status"
---
+++ run_tests.test.bash
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "$script_dir"
+
+
+echo "case: show help message"
+./run_tests.bash --help | grep -qE "^Usage"
+
+
+echo "case: test success"
+# given:
+rm -rf ./tmp && mkdir ./tmp
+cat > ./tmp/success.test.bash << 'SCRIPT'
+echo "case: success"
+test 1 -eq 1 >&2
+SCRIPT
+# when:
+out=$(./run_tests.bash --logfile ./tmp/success-test.log ./tmp/*.test.bash)
+# then:
+expected=$(cat << EXPECTED
+$(tput setaf 4)test: ./tmp/success.test.bash$(tput sgr0)
+case: success
+$(tput setaf 2)all case passed$(tput sgr0)
+EXPECTED
+)
+diff -u <(echo "$expected") <(echo "$out")
+# then:
+grep -q "+ echo 'case: success'" ./tmp/success-test.log
+
+
+echo "case: test fail"
+# given:
+rm -rf ./tmp && mkdir ./tmp
+cat > ./tmp/fail.test.bash << 'SCRIPT'
+echo "case: fail"
+test 1 -eq 2 >&2
+SCRIPT
+# when:
+if out=$(./run_tests.bash --logfile ./tmp/fail-test.log ./tmp/*.test.bash); then
+  echo "error: command should fail" >&2
+  exit 1
+fi
+# then:
+expected=$(cat << EXPECTED
+$(tput setaf 4)test: ./tmp/fail.test.bash$(tput sgr0)
+case: fail
+$(tput setaf 1)failed$(tput sgr0)
++ echo 'case: fail'
++ test 1 -eq 2
+EXPECTED
+)
+diff -u <(echo "$expected") <(echo "$out")
+# then:
+grep -q "+ echo 'case: fail'" ./tmp/fail-test.log
+
+
+echo "case: run all scripts but fail if any test fail"
+# given:
+rm -rf ./tmp && mkdir ./tmp
+cat > ./tmp/success.test.bash << 'SCRIPT'
+echo "case: success"
+test 1 -eq 1 >&2
+SCRIPT
+cat > ./tmp/fail.test.bash << 'SCRIPT'
+echo "case: fail"
+test 1 -eq 2 >&2
+SCRIPT
+# when:
+if out=$(./run_tests.bash --logfile ./tmp/fail-test.log ./tmp/{fail,success}.test.bash); then
+  echo "error: command should fail" >&2
+  exit 1
+fi
+# then:
+expected=$(cat << EXPECTED
+$(tput setaf 4)test: ./tmp/fail.test.bash$(tput sgr0)
+case: fail
+$(tput setaf 1)failed$(tput sgr0)
++ echo 'case: fail'
++ test 1 -eq 2
+
+$(tput setaf 4)test: ./tmp/success.test.bash$(tput sgr0)
+case: success
+$(tput setaf 2)all case passed$(tput sgr0)
+EXPECTED
+)
+diff -u <(echo "$expected") <(echo "$out")
+# then:
+grep -q "+ echo 'case: success'" ./tmp/fail-test.log
+grep -q "+ echo 'case: fail'" ./tmp/fail-test.log
